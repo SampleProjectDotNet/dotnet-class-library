b7dc56f403017e0a00f2b25fba7d62d29b6f418c
Matheusbarros54 <matheus.barros@sptech.school>
2025-05-07 13:26:33 -0300

    feat: dotnet class library cpf validator application

---
+++ ClassLibrary.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClassLibrary.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibrary.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd43d9a9-8d52-436a-a9cb-ce6757a694a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ClassLibrary.Test/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace ClassLibrary.Tests
+{
+    [TestClass]
+    public class CpfValidatorTests
+    {
+        private readonly CpfValidator _validator = new CpfValidator();
+
+        [TestMethod]
+        public void IsValid_ValidCpf_ReturnsTrue()
+        {
+            // Arrange
+            string validCpf = "123.456.789-09"; // Substitua por um CPF válido real
+
+            // Act
+            bool result = _validator.IsValid(validCpf);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsValid_InvalidCpf_ReturnsFalse()
+        {
+            // Arrange
+            string invalidCpf = "123.456.789-00";
+
+            // Act
+            bool result = _validator.IsValid(invalidCpf);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsValid_EmptyCpf_ReturnsFalse()
+        {
+            // Arrange
+            string emptyCpf = "";
+
+            // Act
+            bool result = _validator.IsValid(emptyCpf);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsValid_CpfWithAllSameDigits_ReturnsFalse()
+        {
+            // Arrange
+            string sameDigitsCpf = "111.111.111-11";
+
+            // Act
+            bool result = _validator.IsValid(sameDigitsCpf);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsValid_CpfWithInvalidCharacters_ReturnsFalse()
+        {
+            // Arrange
+            string invalidCpf = "123.abc.789-09";
+
+            // Act
+            bool result = _validator.IsValid(invalidCpf);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
